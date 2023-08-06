3bfffcaee6f9da488bad3ba5a10cedaeb73df047
winter <jin-_-x@naver.com>
2023-08-06 21:39:01 +0900

    gpt

--- java-gpt/src/main/java/com/infoworld/App.java
+++ java-gpt/src/main/java/com/infoworld/App.java
@@ -1,13 +1,34 @@
 package com.infoworld;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        // Set ChatGPT endpoint and API key
+        String endpoint = "https://api.openai.com/v1/chat/completions";
+        String apiKey = "<YOUR-API-KEY>";
+
+        // Prompt user for input string
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter your message: ");
+            String input = reader.readLine();
+
+            // Send input to ChatGPT API and display response
+            String response = ChatBot.sendQuery(input, endpoint, apiKey);
+            LOGGER.info("Response: {}", response);
+        } catch (IOException e) {
+            LOGGER.error("Error reading input: {}", e.getMessage());
+        } catch (JSONException e) {
+            LOGGER.error("Error parsing API response: {}", e.getMessage());
+        } catch (Exception e) {
+            LOGGER.error("Unexpected error: {}", e.getMessage());
+        }
     }
 }
